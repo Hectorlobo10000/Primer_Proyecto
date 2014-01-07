e6f2a8a259a03c981cacd279b58728012299b10d
Hector Lobo <hector.lobo10000@gmail.com>
2014-01-07 17:59:10 +0100

    Interface

--- Primera.Libreria/Suma.cs
+++ Primera.Libreria/Suma.cs
@@ -1,18 +1,5 @@
 namespace Primera.Libreria
 {
-    public interface IOperacion
-    {
-        double Operar(int pnumero, int snumero);
-    }
-
-    public class Resta : IOperacion
-    {
-        public double Operar(int pnumero, int snumero)
-        {
-            return pnumero - snumero;
-        }
-    }
-
     public class Suma : IOperacion
     {
         public double Operar(int pnumero, int snumero)
--- Primera_Consola/Program.cs
+++ Primera_Consola/Program.cs
@@ -9,17 +9,20 @@ namespace Primera_Consola
     {
         private static void Main(string[] args)
         {
-            int pnumero = 0;
-            int snumero = 0;
             Console.Title = "Suma de numeros";
+
             Console.WriteLine("Ingrese el primer numero: ");
-            pnumero = Convert.ToInt32(Console.ReadLine());
+            var pnumero = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Ingrese Operador");
             var operador = Console.ReadLine();
+
             Console.WriteLine("Ingrese el segundo numero: ");
-            snumero = Convert.ToInt32(Console.ReadLine());
+            var snumero = Convert.ToInt32(Console.ReadLine());
+
             var fabrica = ConfiguracionOperacion.ObtenerFabrica();
-            var operacion =fabrica.CrearOperacion(operador);
+            var operacion = fabrica.CrearOperacion(operador);
+
             Console.WriteLine("El resultado es: " + operacion.Operar(pnumero, snumero));
             Console.ReadKey();
         }
